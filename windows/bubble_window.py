f954b95d408686fcf44fb910877d686e91bbbaa1
sinc865 <64197531+ripiripi@users.noreply.github.com>
2024-07-03 18:09:19 +0900

    バルーンウィンドウのサイズをメッセージに合わせて変更できるように

--- windows/bubble_window.py
+++ windows/bubble_window.py
@@ -10,7 +10,11 @@ import random
 class BubbleWindow(WindowBase):
     def __init__(self, root, x_pos, y_pos):
         self.window_width = 300 + 30  # 30 is for padding
+        self.window_height_default = 100
         self.window_height = 100
+        self.balloon_color = "#EFFBFB"
+        self.font = ("Helvetica Neue", 10)  # ("San Francisco", 10)
+
         super().__init__(
             root,
             "吹き出しウィンドウ",
@@ -27,6 +31,15 @@ class BubbleWindow(WindowBase):
         # ウィンドウの透過色を設定
         self.window.attributes("-transparentcolor", self.transparent_color)
 
+        # Canvasウィジェットを作成、背景を透過させる
+        self.canvas = tk.Canvas(
+            self.window,
+            width=self.window_width,
+            height=self.window_height,
+            bg=self.transparent_color,
+            highlightthickness=0,
+        )
+        self.canvas.pack()
         self.set_balloons()
 
         # 初回のみキー生成
@@ -37,38 +50,62 @@ class BubbleWindow(WindowBase):
         self.update_sns_posts()
 
     def set_balloons(self):
-        # Canvasウィジェットを作成、背景を透過させる
-        self.canvas = tk.Canvas(
-            self.window,
-            width=self.window_width,
-            height=self.window_height,
-            bg=self.transparent_color,
-            highlightthickness=0,
-        )
-        self.canvas.pack()
 
+        # 既存のラベルを削除
+        self.canvas.delete("balloon")
+        self.window.geometry(f"{self.window_width}x{self.window_height}")
+        self.canvas.config(height=self.window_height)
+
+        hukidasi_height = 55
+        if self.window_height < hukidasi_height:
+            hukidasi_height = self.window_height / 2
         # 吹き出しの尾部となる三角形を描画
         triangle = [
-            (300, 50),  # バルーンの右端から出るように調整
-            (310, 55),  # 三角形の先端をバルーンの中心線に合わせる
-            (300, 60),
+            (300, hukidasi_height - 5),  # バルーンの右端から出るように調整
+            (310, hukidasi_height),  # 三角形の先端をバルーンの中心線に合わせる
+            (300, hukidasi_height + 5),
         ]
-        self.canvas.create_polygon(triangle, fill="ebffff", outline="ebffff")
+        balloon_color = self.balloon_color
+        self.canvas.create_polygon(triangle, fill=balloon_color, outline=balloon_color, tags="balloon")
         # バルーンの本体部分を描画
         # self.canvas.create_rectangle(0, 0, 300, 100, fill="white", outline="white")
         radius = 10  # 角の丸みの半径
 
         # 角が丸い長方形を描画するために、四隅に円を描く
-        self.canvas.create_oval(0, 0, radius * 2, radius * 2, fill="ebffff", outline="ebffff")  # 左上の角
-        self.canvas.create_oval(300 - radius * 2, 0, 300, radius * 2, fill="ebffff", outline="ebffff")  # 右上の角
-        self.canvas.create_oval(0, 100 - radius * 2, radius * 2, 100, fill="ebffff", outline="ebffff")  # 左下の角
         self.canvas.create_oval(
-            300 - radius * 2, 100 - radius * 2, 300, 100, fill="ebffff", outline="ebffff"
+            0, 0, radius * 2, radius * 2, fill=balloon_color, outline=balloon_color, tags="balloon"
+        )  # 左上の角
+        self.canvas.create_oval(
+            300 - radius * 2, 0, 300, radius * 2, fill=balloon_color, outline=balloon_color, tags="balloon"
+        )  # 右上の角
+        self.canvas.create_oval(
+            0,
+            self.window_height - radius * 2,
+            radius * 2,
+            self.window_height,
+            fill=balloon_color,
+            outline=balloon_color,
+            tags="balloon",
+        )  # 左下の角
+        self.canvas.create_oval(
+            300 - radius * 2,
+            self.window_height - radius * 2,
+            300,
+            self.window_height,
+            fill=balloon_color,
+            outline=balloon_color,
+            tags="balloon",
         )  # 右下の角
 
         # 中央の長方形と上下の長方形を描画して、角が丸い長方形を完成させる
-        self.canvas.create_rectangle(radius, 0, 300 - radius, 100, fill="ebffff", outline="ebffff")  # 中央
-        self.canvas.create_rectangle(0, radius, 300, 100 - radius, fill="ebffff", outline="ebffff")  # 上下
+        self.canvas.create_rectangle(
+            radius, 0, 300 - radius, self.window_height, fill=balloon_color, outline=balloon_color, tags="balloon"
+        )  # 中央
+        self.canvas.create_rectangle(
+            0, radius, 300, self.window_height - radius, fill=balloon_color, outline=balloon_color, tags="balloon"
+        )  # 上下
+
+        self.canvas.tag_lower("all")
 
     def bluesky_login(self):
 
@@ -87,19 +124,41 @@ class BubbleWindow(WindowBase):
         self.client.send_post(text="テスト")
 
     def update_sns_posts(self):
+
         response = self.client.get_timeline()
         rand_int = random.randint(0, 50)
         tl = {"feed": response.feed[rand_int]}
         post_text = tl["feed"].post.record.text
-        # label = tk.Label(self.window, text=post_text, wraplength=self.window.winfo_width() - 20)  # 20 is for padding
-        # label.pack(padx=10, pady=10)  # Set padding around the label
-        # label = tk.Label(self.window, text=post_text)
-        # label.pack()
+        post_text = "テストおおおおおおおおお\nおおおお\nお\nお\nお\nお\nお\nお\nお\nお\nお\nお\nお\nお\nお\nお\nお\nお\nおおおおおお"
+
+        # 既存のラベルを削除
+        self.canvas.delete("post_text")
+
+        # ラベルを作成して追加
         label = tk.Label(
-            self.window, text=post_text, wraplength=self.window.winfo_width() - 20, anchor="w", justify="left"
+            self.canvas,
+            text=post_text,
+            wraplength=self.window_width - 50,
+            anchor="nw",
+            justify="left",
+            bg=self.balloon_color,
+            font=self.font,
         )
-        label.pack(padx=10, pady=10, anchor="w")
-        # self.window.after(300000, self.update_sns_posts)
+        self.canvas.update_idletasks()  # レイアウトを確定
+
+        self.canvas.create_window(10, 10, window=label, anchor="nw", tags="post_text")
+
+        label_height = label.winfo_reqheight()
+
+        # サイズを確認
+        print(f"height: {label_height}")
+
+        # バルーンの高さがをlabel_height+20になるように調整し、再描画
+        self.window_height = label_height + 20
+        self.set_balloons()
+        # TODO: バルーンの高さを自動調整する
+
+        self.window.after(300000, self.update_sns_posts)
 
     def display_settings(self):
         self.window.lift()
